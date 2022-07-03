39e571c5d235c368b1cb7d324220a989614b30ec
WahyuDwe <wahyu5161.w5@gmail.com>
2022-07-03 09:24:45 +0700

    add scrolling to down when click item on navbar

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('ul.nav a').bind('click', function(event) {
+        event.preventDefault();
+        var $anchor = $(this);
+        console.log($anchor.attr('href'))
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000);
+        event.preventDefault();
+    })
+})
